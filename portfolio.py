158983915a1e299e87a9caf5a8c8a0fe5ed5aaf3
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2021-02-28 14:15:10 -0600

    merged testing

--- portfolio.py
+++ portfolio.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
-def home():
-    return render_template('home.html')
+def webpage():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
